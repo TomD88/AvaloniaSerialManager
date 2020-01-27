a272e1e50bce67048e267ad1c5b1766bc1f49f5b
TomD88 <cto.desiato@gmail.com>
2020-01-27 23:02:52 +0100

    Add project files.

---
+++ AvaloniaSerialManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using ReactiveUI;
+
+namespace AvaloniaSerialManager.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+        
+        private Timer _stateTimer;
+
+        private string _serialPortsString;
+        public string SerialPortsString
+        {
+            get => _serialPortsString;
+            set => this.RaiseAndSetIfChanged(ref _serialPortsString, value);
+        }
+
+
+        private SerialPort _serialPort;
+
+        public MainWindowViewModel()
+        {
+            _serialPort = new SerialPort();
+
+
+            _stateTimer = new Timer(UpdateAvailableSerialPorts,null,Timeout.Infinite,5000);
+            
+            
+        }
+
+        internal void DisposeInternal()
+        {
+            if (_stateTimer != null)
+                 _stateTimer.Dispose();
+        }
+
+        public void UpdateAvailableSerialPorts(Object state) {
+
+            var availableSerialPortsNames = String.Empty;
+            Debug.WriteLine("[UpdateAvailableSerialPorts]Called");
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Debug.WriteLine($"{s}");
+                if (!string.IsNullOrEmpty(s))
+                    availableSerialPortsNames=string.Join(" ", availableSerialPortsNames, s);
+            }
+
+            SerialPortsString = availableSerialPortsNames;
+
+        }
+
+        public void GetSerialPortsCommand()
+        {
+            //foreach (string s in SerialPort.GetPortNames())
+            //{
+            //    Console.WriteLine("   {0}", s);
+            //}
+
+            Debug.WriteLine("[GetSerialPorts]Called");
+
+
+            _stateTimer.Change(0, 5000);
+
+            //Content = new AddItemViewModel();
+        }
+    }
+}
---
+++ AvaloniaSerialManager/Views/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using AvaloniaSerialManager.ViewModels;
+
+namespace AvaloniaSerialManager.Views
+{
+    public class MainWindow : Window
+    {
+
+        public Button GetSerialPortsButton => this.FindControl<Button>("GetSerialPortsButton");
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+
+            DataContext = new MainWindowViewModel();
+
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.DataContext != null)
+                ((MainWindowViewModel)DataContext).DisposeInternal();
+
+
+            this.Closing -= MainWindow_Closing;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
